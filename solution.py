8897466e8c561d41bafb749b6f969873e9c7cde5
Cariburi <nbokamoso5@gmail.com>
2025-06-17 23:50:03 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,14 +2,14 @@ def birthdayCakeCandles(candles: list[int]) -> int:
     if len(candles) == 0:
         return 0
     
-    big_candle = []
-    biggest_candle = max(candle)
+    big_candles = []
+    biggest_candle = max(candles)
     
     for i in candles:
         if i == biggest_candle:
-            biggest_candle.append(i)
+            big_candles.append(i)
         
         else:
             continue
     
-    return len(biggest_candle)
+    return len(big_candles)
